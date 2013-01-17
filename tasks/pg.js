e12ee0d400c8cdec7c7e57757f05373677bc0b2d
Andrew Chilton <chilts@appsattic.com>
2013-01-17 17:57:14 +1300

    Add a target to be able to run an SQL file

--- tasks/pg.js
+++ tasks/pg.js
@@ -34,6 +34,8 @@ module.exports = function(grunt) {
   // Please see the grunt documentation for more information regarding task
   // creation: https://github.com/gruntjs/grunt/blob/devel/docs/toc.md
 
+  var _ = grunt.utils._;
+
   grunt.registerMultiTask('pgcreateuser', 'Add a new Postgres user.', function() {
       var self = this;
       var data = self.data;
@@ -90,13 +92,58 @@ module.exports = function(grunt) {
       });
   });
 
-  grunt.registerMultiTask('pgdropdb', 'Drop a Postgres database..', function() {
+  grunt.registerMultiTask('pgdropdb', 'Drop a Postgres database.', function() {
       var self = this;
       var done = this.async();
       var data = this.data;
 
       execute_db(data.connection, "DROP DATABASE IF EXISTS " + data.name + ";", function(err, res) {
           grunt.log.writeln('Database "' + data.name + '" dropped.');
+          done();
+      });
+  });
+
+  grunt.registerMultiTask( 'pgsqlfile', 'Run a sql against a Postgres database', function() {
+      var exec = require('child_process').exec;
+      var done = this.async();
+      var data = _.extend( [], grunt.config.get('shell')._options, this.data );
+      var dataOut = data.stdout;
+      var dataErr = data.stderr;
+
+      if ( _.isFunction( data.callback ) ) {
+          data.callback.call( this );
+          return;
+      }
+
+      var db = data.connection;
+      var command = (db.password ? "PGPASSWORD='" + db.password + "'" : "") +
+        "psql " +
+        " -d " + db.name +
+        (db.host ? " -h " + db.host : "" ) +
+        " -p " + db.port +
+        " -U " + db.user +
+        " -w " +
+        " -f " + data.filename;
+
+      exec( grunt.template.process(command), data.execOptions, function( err, stdout, stderr ) {
+          if ( stdout ) {
+              if ( _.isFunction( dataOut ) ) {
+                  dataOut( stdout );
+              } else if ( dataOut === true ) {
+                  grunt.log.write( stdout );
+              }
+          }
+
+          if ( err ) {
+              if ( _.isFunction( dataErr ) ) {
+                  dataErr( stderr );
+              } else if ( data.failOnError === true ) {
+                  grunt.fatal( err );
+              } else if ( dataErr === true ) {
+                  grunt.log.error( err );
+              }
+          }
+
           done();
       });
   });
